5d3ec9bfdace3ce339b9ae319546b58afd94ce1f
hbybyyang <2451759073@qq.com>
2022-02-05 14:36:21 +0800

    ws连接上才返回

--- src/Lib/Ws.js
+++ src/Lib/Ws.js
@@ -2,6 +2,8 @@ exports.getWsHard = (conf) => () => {
   return new Promise((res, rej) => {
     var WebSocket = require("ws");
     var ws = new WebSocket(`ws://${conf.addr}:${conf.port}`);
-    res(ws);
+    ws.on("open", function open() {
+      res(ws);
+    });
   });
 };
